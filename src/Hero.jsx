99e5bba18ea56e5fb5ff337e0be4d5e55fdb37b3
Hari-Hara-2005 <gharanthasec@gmail.com>
2024-06-27 22:20:07 +0530

    Change_flipword

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -13,9 +13,9 @@ const Hero = () => {
   return (
     <>
       <Navbar />
-      <StickyWhatsapp link={"https://wa.me/916369842254"} />
+      <StickyWhatsapp link={"https://wa.me/8220570301"} />
       {/* Hero Section */}
-      <Box className="hero_bg" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Box className="hero_bg" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', overflow: 'hidden' }}>
         <Box>
           <Typography sx={{
             fontSize: ['2.4rem', '4.5rem',],
@@ -26,19 +26,19 @@ const Hero = () => {
           }}>Maha's Lettering</Typography>
 
           <Typography sx={{ textAlign: 'center', fontSize: ['1rem', '1.4rem'], fontWeight: 'bold', my: 1.5, px: 1, color: 'white', mb: '5rem' }}>Crafting is the art of turning ordinary objects into extraordinary things</Typography>
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'center', overflow: 'hidden' }}>
             <Typography sx={{
               fontSize: ['1.5rem', '1.5rem', '2.5rem'],
               fontWeight: '600',
               color: 'white',
               bgcolor: 'transparent',
               textAlign: 'center',
-              display: 'flex',
-              color: '#fff'
+              color: '#fff',
+              display: "flex",
             }}>
               We Provide&nbsp;
             </Typography>
-            {/* <FlipWordsDemo /> */}
+            <FlipWordsDemo />
           </Box>
         </Box>
       </Box>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,30 +13,30 @@ const Footer = () => {
             {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FF6B6B" fillOpacity="1" d="M0,32L80,53.3C160,75,320,117,480,112C640,107,800,53,960,42.7C1120,32,1280,64,1360,80L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg> */}
             <Box sx={{ display: 'flex', justifyContent: 'center', my: ['0rem', '1.5rem'], }}>
                 <Box sx={{ bgcolor: '#282828', width: ['7rem', '7rem', '9rem'], height: ['7rem', '7rem', '9rem'], display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '100%' }}>
-                    <Box component='img' src='/logoh.png' alt="customer" sx={{ width: ['8rem', '8rem', '10.5rem'], height: ['8rem', '8rem', '10.5rem'], borderRadius: '100%' }} />
+                    <Box component='img' src='/logoh.png' alt="customer" sx={{ width: ['8rem', '8rem', '10.5rem'], ml: 0.5, height: ['8rem', '8rem', '10.5rem'], borderRadius: '100%' }} />
                 </Box>
             </Box>
             <Box sx={{ backgroundColor: '#282828', mt: { xs: -8, sm: -12, md: -12 }, pb: 2 }}>
-                <Box sx={{ pt: 8, mx: 4 }}>
-                    <Typography sx={{ fontSize: '18px', fontFamily: 'sans-serif', fontWeight: '500', color: 'white', letterSpacing: 1 }}>RED PANDA</Typography>
-                    <Typography sx={{ fontSize: '10px', fontFamily: 'sans-serif', fontWeight: '500', color: 'white', letterSpacing: 7 }}>VACATIONS</Typography>
+                <Box sx={{ pt: 8, mx: [2.5, 4] }}>
+                    <Typography sx={{ fontSize: '18px', fontFamily: 'sans-serif', fontWeight: '500', color: 'white', letterSpacing: 1, textTransform: 'uppercase', mx: 2, my: 0.5 }}>Maha's</Typography>
+                    <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', fontWeight: '500', color: 'white', letterSpacing: 5, textTransform: 'uppercase' }}>Lettering</Typography>
                 </Box>
 
-                <Box sx={{ mx: 4, mt: 4 }}>
+                <Box sx={{ mx: [2.5, 4], mt: 3 }}>
                     <Grid container spacing={6} justifyContent={{ xs: 'flex-start', md: 'center' }}>
                         <Grid item xs={12} sm={6} lg={3}>
                             <Box>
                                 <Typography sx={{ fontSize: '18px', fontFamily: 'sans-serif', fontWeight: '500', color: 'white', letterSpacing: 1 }}>
                                     Address
                                 </Typography>
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', mt: 2, letterSpacing: 1 }}>
-                                    No-10, Third Floor, 2nd Main Rd,
+                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', mt: 1, letterSpacing: 1 }}>
+                                    X97F+92G, Chakrapani Sannathi Street,
                                 </Typography>
                                 <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', letterSpacing: 1 }}>
-                                    Balaji Nagar, Ekkatuthangal,
+                                    Karna Kollai Agraharam, Valayapettai Agraharam,
                                 </Typography>
                                 <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', letterSpacing: 1 }}>
-                                    Chennai, Tamil Nadu 600032, India.
+                                    Kumbakonam, Tamil Nadu 612001, India.
                                 </Typography>
                             </Box>
                         </Grid>
@@ -45,11 +45,8 @@ const Footer = () => {
                                 <Typography sx={{ fontSize: '18px', fontFamily: 'sans-serif', fontWeight: '500', color: 'white', letterSpacing: 1 }}>
                                     Contact Us
                                 </Typography>
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', mt: 2, letterSpacing: 1, display: 'flex', alignItems: 'center', gap: 1 }}>
-                                    <MailIcon />contact@redpandavacations.com
-                                </Typography>
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', mt: 0.5, letterSpacing: 1, display: 'flex', alignItems: 'center', gap: 1 }}>
-                                    <PhoneIcon />96298 79633
+                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', mt: 1, letterSpacing: 1, display: 'flex', alignItems: 'center', gap: 1 }}>
+                                    <MailIcon />maha050108@gmail.com
                                 </Typography>
                             </Box>
                         </Grid>
@@ -58,23 +55,17 @@ const Footer = () => {
                                 <Typography sx={{ fontSize: '18px', fontFamily: 'sans-serif', fontWeight: '500', color: 'white', letterSpacing: 1 }}>
                                     Overview
                                 </Typography>
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', letterSpacing: 1, mt: 2 }}>
+                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', letterSpacing: 1, mt: 1 }}>
+                                    Home
+                                </Typography>
+                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', letterSpacing: 1, mt: 1 }}>
                                     About
                                 </Typography>
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', letterSpacing: 1 }}>
-                                    Customer-Support
+                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', letterSpacing: 1, mt: 1 }}>
+                                    Product
                                 </Typography>
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', letterSpacing: 1 }}>
-                                    Cancellation-and-refund-policy
-                                </Typography>
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', letterSpacing: 1 }}>
-                                    Policy
-                                </Typography>
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', letterSpacing: 1 }}>
-                                    Services
-                                </Typography>
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', letterSpacing: 1 }}>
-                                    Terms-condition
+                                <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', color: 'white', letterSpacing: 1, mt: 1 }}>
+                                    Contact Us
                                 </Typography>
                             </Box>
                         </Grid>
@@ -85,15 +76,30 @@ const Footer = () => {
                                 </Typography>
                                 <Box sx={{ mt: 2 }}>
                                     <Fab component='a'
-                                        href='https://wa.me/916369842254' sx={{ backgroundColor: 'white', mx: 0.5, color: '#282828' }}>
+                                        href='https://wa.me/8220570301' sx={{ backgroundColor: 'white', mx: 0.5, color: '#282828' }}>
                                         <WhatsAppIcon />
                                     </Fab>
                                     <Fab component='a'
-                                        href='https://www.instagram.com/chan_dru472?igsh=Z2UxMmR2M3Bib3U=' sx={{ backgroundColor: 'white', mx: 0.5, color: '#282828' }}>
+                                        href='https://www.instagram.com/mahaslettering?igsh=b21mbmJyY2hqd2Vo' sx={{ backgroundColor: 'white', mx: 0.5, color: '#282828' }}>
                                         <InstagramIcon />
                                     </Fab>
-                                    <Fab sx={{ backgroundColor: 'white', mx: 0.5, color: '#282828' }}>
+                                    <Fab component='a'
+                                        href='https://www.facebook.com/mahalakshmi.shankaran.54?mibextid=ZbWKwL' sx={{ backgroundColor: 'white', mx: 0.5, color: '#282828' }}>
                                         <FacebookIcon />
+                                    </Fab>
+                                    <Fab component='a'
+                                        href='https://www.threads.net/@mahaslettering' sx={{ backgroundColor: 'white', mx: 0.5, color: '#282828' }}>
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                            shape-rendering="geometricPrecision"
+                                            text-rendering="geometricPrecision"
+                                            image-rendering="optimizeQuality"
+                                            fill-rule="evenodd"
+                                            clip-rule="evenodd"
+                                            viewBox="0 0 512 512"
+                                            width="25"
+                                            height="25"
+                                            backgroundColor='#fff'
+                                            style={{ backgroundColor: 'transparent' }}><path d="M105 0h302c57.75 0 105 47.25 105 105v302c0 57.75-47.25 105-105 105H105C47.25 512 0 464.75 0 407V105C0 47.25 47.25 0 105 0z" /><path fill="#fff" fill-rule="nonzero" d="M337.36 243.58c-1.46-.7-2.95-1.38-4.46-2.02-2.62-48.36-29.04-76.05-73.41-76.33-25.6-.17-48.52 10.27-62.8 31.94l24.4 16.74c10.15-15.4 26.08-18.68 37.81-18.68h.4c14.61.09 25.64 4.34 32.77 12.62 5.19 6.04 8.67 14.37 10.39 24.89-12.96-2.2-26.96-2.88-41.94-2.02-42.18 2.43-69.3 27.03-67.48 61.21.92 17.35 9.56 32.26 24.32 42.01 12.48 8.24 28.56 12.27 45.26 11.35 22.07-1.2 39.37-9.62 51.45-25.01 9.17-11.69 14.97-26.84 17.53-45.92 10.51 6.34 18.3 14.69 22.61 24.73 7.31 17.06 7.74 45.1-15.14 67.96-20.04 20.03-44.14 28.69-80.55 28.96-40.4-.3-70.95-13.26-90.81-38.51-18.6-23.64-28.21-57.79-28.57-101.5.36-43.71 9.97-77.86 28.57-101.5 19.86-25.25 50.41-38.21 90.81-38.51 40.68.3 71.76 13.32 92.39 38.69 10.11 12.44 17.73 28.09 22.76 46.33l28.59-7.63c-6.09-22.45-15.67-41.8-28.72-57.85-26.44-32.53-65.1-49.19-114.92-49.54h-.2c-49.72.35-87.96 17.08-113.64 49.73-22.86 29.05-34.65 69.48-35.04 120.16v.24c.39 50.68 12.18 91.11 35.04 120.16 25.68 32.65 63.92 49.39 113.64 49.73h.2c44.2-.31 75.36-11.88 101.03-37.53 33.58-33.55 32.57-75.6 21.5-101.42-7.94-18.51-23.08-33.55-43.79-43.48zm-76.32 71.76c-18.48 1.04-37.69-7.26-38.64-25.03-.7-13.18 9.38-27.89 39.78-29.64 3.48-.2 6.9-.3 10.25-.3 11.04 0 21.37 1.07 30.76 3.13-3.5 43.74-24.04 50.84-42.15 51.84z" /></svg>
                                     </Fab>
                                 </Box>
                             </Box>
@@ -103,7 +109,7 @@ const Footer = () => {
 
                 <Box>
                     <Typography sx={{ fontSize: '15px', fontFamily: 'sans-serif', fontWeight: '600', color: 'white', letterSpacing: 1, mt: 6, textAlign: 'center' }}>
-                        Red Panda vacations © 2024 | Developed By Chandru
+                        Maha's Lettering © 2024
                     </Typography>
                 </Box>
             </Box>
